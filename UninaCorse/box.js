ac9b3a243e5e100341d278d223de4f92c160c9d2
Nicovic17 <57710768+Nicovic17@users.noreply.github.com>
2020-02-05 12:02:30 +0100

    Suddivisioni delle funzionalità in "classi"
    Ad ogni elemento è ora associata una propria classe che ne gestisce grafica e valori.
    La classe BOX.JS da ora si occuperà esclusivamente della connessione al database per l'aggiornamento di valori.
    
    Da sistemare il grafico break.

--- UninaCorse/box.js
+++ UninaCorse/box.js
@@ -31,29 +31,6 @@ firebaseTemp.on("value", function (datasnapshot) {
     
 });
 
-function updatePlotTemp()
-{
-    myPlot(tempIniz);
-
-    setTimeout(function(){
-
-        updatePlotTemp();
-
-    },1000);
-
-}
-
-function startPlot(val)
-{
-    Plotly.plot('chartContainer',[{
-        y:[val],
-        type:'line'
-    }]);
-}
-
-startPlot(tempIniz);
-updatePlotTemp();
-
 
 let throttle;
 let speed;
@@ -84,76 +61,12 @@ fireBaseBreak.on("value", function (datasnapshot) {
 	drawBreakValue(breakk,throttle);
 });
 
-var cont=0;
-
-function myPlot(val)
-{
-    
-    cont++;
-    Plotly.extendTraces('chartContainer',{ y:[[val]]},[0]);
-
-    if(cont>10)
-    {
-        Plotly.relayout('chartContainer',{
-            xaxis: {
-                range: [cont-10,cont]
-            }
-        });
-    }
-
-    /*setInterval(function(){
-        
-        Plotly.extendTraces('chartContainer',{ y:[[val]]},[0]);
-
-        cnt++;
-
-        if(cnt>500){
-            Plotly.relayout('chartContainer',{
-                xaxis: { 
-                    range:[cnt-500,cnt]
-                }
-            });
-        }
-
-    },1000);*/
-}
 
 
-function myBattery(lvl)
-{
-    let battery=document.getElementById("battery");
 
-    if(lvl<=100 && lvl>75)
-    {
-        battery.innerHTML = "&#xf240";
-    }
-    else
-    if(lvl<=75 && lvl >50)
-    {
-        battery.innerHTML = "&#xf241";
-    }
-    else
-    if(lvl<=50 && lvl >25)
-    {
-        battery.innerHTML = "&#xf242";
-    }
-    else
-    if(lvl<=25 && lvl>0)
-    {
-        battery.innerHTML = "&#xf243";
-    }
-    else
-    if(lvl==0)
-    {
-        battery.innerHTML = "&#xf244";
-    }
 
-    /*setTimeout(function(){
-        lvl=lvl-25;
-        myBattery(lvl);
-    },1000);*/
-    
-}
+
+
 
 
 
